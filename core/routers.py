8026c2e2adae63e40ac69ca76f3a5df9406321d4
arcenioCorteis <arcenioCorteis@github.com>
2024-10-06 02:36:52 +0200

    feat: Login with user credentials

--- core/routers.py
+++ core/routers.py
@@ -1,6 +1,6 @@
 from rest_framework import routers
 from core.user.viewsets import UserViewSet
-from core.auth.viewsets.register import RegisterViewSet
+from core.auth.viewsets import RegisterViewSet, LoginViewSet
 
 router = routers.SimpleRouter()
 
@@ -11,6 +11,7 @@ router.register(r'user', UserViewSet, basename='user')
 
 # ####### AUTH
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
+router.register(r'auth/login', LoginViewSet, basename='auth-login')
 
 urlpatterns = [
     *router.urls
